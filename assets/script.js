8df5499ce1f1f048068355601d7326d37d0b3e3c
Andrew Scala <agscala@gmail.com>
2012-07-26 18:19:50 -0400

    Fixing fucking blog post

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,11 @@
-function animateRandomColor(div) {
-	var callback = (function(div) { return function(div) { animateRandomColor(div); } })(div);
-	var color = Math.floor(Math.random() * 100) + "";
-
-	console.log(color);
-	$(this).animate({"color": "rgb(" + color + ", 232, 49)"}, 1000, function() {
-		animateRandomColor(div);
-	});
-}
-
 $(document).ready(function() {
-	var logo_text = $("h1 a").html().split("");
-	$("h1 a").empty();
-	console.log(logo_text);
-	_.each(logo_text, function(character) {
-		$("h1 a").append("<span class='color-trans'>" + character + "</span>");
+	var bodytext = $("body").html();
+	$("code").each(function(i, elem) {
+		var text = $(this).html();
+		text = text.replace("|{|", "{{");
+		text = text.replace("|}|", "}}");
+		console.log(text);
+		$(this).html(text);
 	});
-
-	animateRandomColor(".color-trans");
-	// $(".color-trans").each(function() {
-		// animateRandomColor(this);
-	// });
 });
 
